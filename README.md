5cb3ef372fca5acabab2312cefed7ce11dcb58b6
jem <michael.jerger@meissa-gmbh.de>
2020-04-18 16:56:45 +0200

    clojure init

---
+++ README.md
@@ -0,0 +1,21 @@
+# dda-provision
+[![Clojars Project](https://img.shields.io/clojars/v/dda/dda-provision.svg)](https://clojars.org/dda/dda-provision)
+[![Build Status](https://travis-ci.org/DomainDrivenArchitecture/dda-provision.svg?branch=master)](https://travis-ci.org/DomainDrivenArchitecture/dda-provision)
+
+[![Slack](https://img.shields.io/badge/chat-clojurians-green.svg?style=flat)](https://clojurians.slack.com/messages/#dda-pallet/) | [<img src="https://meissa-gmbh.de/img/community/Mastodon_Logotype.svg" width=20 alt="team@social.meissa-gmbh.de"> team@social.meissa-gmbh.de](https://social.meissa-gmbh.de/@team) | [Website & Blog](https://domaindrivenarchitecture.org)
+
+## Compatability
+
+dda-provision is compatible to the following versions
+ * clojure 1.9
+
+## Purpose
+
+dda-provision provides a rich set of provisioning and devops development supporting tools.
+
+## License
+
+Copyright © 2020 meissa GmbH
+Licensed under the [Apache License, Version 2.0](LICENSE) (the "License")
+Pls. find licenses of our subcomponents [here](doc/SUBCOMPONENT_LICENSE)
+
---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject dda/dda-provision "0.1.0-SNAPSHOT"
+  :description "tools for provisioning"
+  :url "https://www.domaindrivenarchitecture.org"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [dda/dda-config-commons "0.1.0"]]
+  :source-paths ["main/src"]
+  :resource-paths ["main/resources"]
+  :repositories [["snapshots" :clojars]
+                 ["releases" :clojars]]
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]]
+  :profiles {:dev {:source-paths ["test/src"
+                                  "uberjar/src"]
+                   :resource-paths ["test/resources"]
+                   :dependencies
+                   [[dda/data-test "0.1.1"]]
+                   :leiningen/reply
+                   {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
+                    :exclusions [commons-logging]}}
+             :test {:test-paths ["test/src"]
+                    :resource-paths ["test/resources"]
+                    :dependencies [[dda/data-test "0.1.1"]]}}
+  :local-repo-classpath true)
