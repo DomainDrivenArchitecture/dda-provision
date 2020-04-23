bb1966721a0c39b2701bb8efc277325b21749d2b
jem <michael.jerger@meissa-gmbh.de>
2020-04-23 16:35:30 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-provision "0.1.0"
+(defproject dda/dda-provision "0.1.1-SNAPSHOT"
   :description "tools for provisioning"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
